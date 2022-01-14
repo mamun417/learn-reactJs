7c4ad28f148545fb2924ef60ee15fbddb1c9943d
abdullah.m <abdullah.m@bitmascot.com>
2022-01-15 00:09:21 +0600

    Lifting State Up

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,23 +1,28 @@
 import React from "react";
 import BoilingVerdict from "./BoilingVerdict";
+import TemperatureInput from "./TemperatureInput";
+import { toCelsius, toFahrenheit, tryConvert } from "./lib/converter";
 
 export default class Calculator extends React.Component {
     state = { temperature: "", scale: "c" };
 
-    handleChange = (e) => {
+    handleChange = (e, scale) => {
         this.setState({
             temperature: e.target.value,
+            scale,
         });
     };
 
     render() {
-        const { temperature } = this.state;
+        const { temperature, scale } = this.state;
+        const celsius = scale === "f" ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
 
         return (
             <fieldset>
-                <legend>Enter temperature in Celsius:</legend>
-                <input value={temperature} onChange={this.handleChange} />
-                <BoilingVerdict celsius={parseFloat(temperature)} />
+                <TemperatureInput scale="c" temperature={celsius} onChangeHandler={this.handleChange} />
+                <TemperatureInput scale="f" temperature={fahrenheit} onChangeHandler={this.handleChange} />
+                <BoilingVerdict celsius={parseFloat(celsius)} />
             </fieldset>
         );
     }
---
+++ src/components/TemperatureInput.js
@@ -0,0 +1,20 @@
+import React from "react";
+import BoilingVerdict from "./BoilingVerdict";
+
+const scaleNames = {
+    c: "Celsius",
+    f: "Fahrenheit",
+};
+
+export default class TemperatureInput extends React.Component {
+    render() {
+        const { temperature, scale, onChangeHandler } = this.props;
+
+        return (
+            <div>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={temperature} onChange={(e) => onChangeHandler(e, scale)} />
+            </div>
+        );
+    }
+}
